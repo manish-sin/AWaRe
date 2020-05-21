8f7697c2ebed2220dd241efa6f3d8a31b10b2507
manish <manish.singh35200@gmail.com>
2020-05-22 03:58:50 +0530

    stable

--- static/scripts/report.js
+++ static/scripts/report.js
@@ -17,36 +17,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
 		console.log(`Sub-app Name: ${data[1][0]};`) 
 		console.log(`Sub_app Count: ${data[1][3]};`) 
 		
-		/* for (i = 1; i < data[0]+1; i++) {
-         console.log(`Message No.  : ${data[i]};`)
-        } */
+		
 
-
-
-	/* #const data = data */
-	
-	
-	function addData(chart, label, data) {
-		console.log("add function read");
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
-	}
-
-	function removeData(chart) {
-		console.log("remove function read");
-    chart.data.labels.shift();
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.shift();
-		console.log(chart);
-	
-    });
-    chart.update();
-	}
-	
-    
+	var h= data[1][3]*40;
 	
 	var color = ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#1ABC9C", "#A93226", "#F5B7B1", "#633974", "#2471A3", "#AED6F1", "#148F77" ]
 	
@@ -106,48 +79,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 	
 	var chrome_chart = new Chart(document.getElementById("chrome-chart"), b);
-	
-	/* chrome_chart.destroy();
-	removeData(chrome_chart) */
-	
-/* 	ctx = document.getElementById("chrome-chart");
-	ctx.height = 500;
-	chrome_chart = new Chart(ctx, b);
-	console.log(chrome_chart) */
-	
-	
+	chrome_chart.destroy();
+	chrome_chart = new Chart(document.getElementById("chrome-chart"), b);
 
 	
-    
-	/* var c = data[1][0]
-	var d = data[1][1]
-	removeData(chrome_chart)
-	addData(chrome_chart, c, d) */ 
-	
-	
-	
-	
-	
-	
-	
-	/* if(chrome_chart!=null){console.log("did read if")
-		chrome_chart.data[0] = data[1][0];
-	  chrome_chart.data.datasets.data = data[1][1]; // Would update the first dataset's value of 'March' to be 50
-	  chrome_chart.options.title.text=data[1][2];
-	chrome_chart.update();}
-	else{chrome_chart.data[0] = data[1][0];
-	  chrome_chart.data.datasets.data = data[1][1]; // Would update the first dataset's value of 'March' to be 50
-	  chrome_chart.options.title.text=data[1][2];
-	chrome_chart.update();	
-      
-	  
-	console.log("did not read if")
-	}	 */
-	
-	
-
-	
-	document.getElementById('chrome-chart').height = "100"
 	document.getElementById("pie-chart").onclick = function(evt)
 	 {var activePoints = pie_chart.getElementsAtEvent(evt);
       if (activePoints[0]) {
@@ -157,8 +92,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
         var label = chartData.labels[idx];
         var value = chartData.datasets[0].data[idx];
 
-        var url = "http://localhost:5000/detail";
-        console.log(url);
+        
+        
 		
 		
 		socket.emit('msg',{label} );
@@ -166,12 +101,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
 		
 		
 		
+	  }	
+		
+	document.getElementById("go").onclick = function(){
 	
+	var y = document.getElementById("start").value;
+	console.log(`Date: ${y};`);
+	socket.emit('msg',{y});	
+	}
 		
 		
        
-      }
-    };
+      };
 	
 	
 	
