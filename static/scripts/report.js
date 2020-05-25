3777c196a95c6c4b741f6f286ad32c557991f00b
Rahul Singh <rahulsingh190792@gmail.com>
2020-05-25 15:00:56 +0530

    committing the report.js with the feature to display data based on date

--- static/scripts/report.js
+++ static/scripts/report.js
@@ -6,32 +6,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
 		socket.send("I am connected"); 
 	})
 	
-	
-	function go(){
-	  var socket = io.connect('http://localhost:5000');
-	  var y = document.getElementById("start").value;  
-	  socket.emit('msg',{y});
-	  console.log(`Go Home: ${y};`);
-	  
-	  }
-	
-	
-	
-
+	document.querySelector('#go').onclick = () =>{
+		console.log(`Hi`);
+		var y = document.getElementById("start").value;  
+		console.log(`Go Home: ${y};`);
+		socket.emit('msg',{y});
+	};
 	
 	socket.on('message', data=> {
 		<!-- var variable1 = localStorage.getItem("vOneLocalStorage "); */ -->
 		/* console.log(`Apps Names: ${data[0][0]};`)
 		console.log(`Apps Duration: ${data[0][1]};`) */
 		/* console.log(`App Count: ${data[0][2]};`)*/
-		console.log(`Choosen Apps Name: ${data[1][2]};`)
+		/* console.log(`Choosen Apps Name: ${data[1][2]};`) */
 		/* console.log(`Sub-app Duration: ${data[1][1]};`)
-		console.log(`Sub-app Name: ${data[1][0]};`)
-		console.log(`Sub_app Count: ${data[1][3]};`)*/ 
-		console.log(`Today's Date: ${data};`)
+		console.log(`Sub-app Name: ${data[1][0]};`)*/ 
+		console.log(`Sub_app Count: ${data};`)
+		console.log(`Today's Date: ${data[4]};`)
 		console.log(`Max Date: ${data[2]};`)
 		console.log(`Min Date: ${data[3]};`)
-		var recived_date = data[1][4];
+		var recived_date = data[4];
 		document.getElementById("start").value = recived_date;
 		document.getElementById("start").min = data[2]
 		document.getElementById("start").max = data[3]
@@ -97,7 +91,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
 		}
 		
 	};
-	var pie_chart = new Chart(document.getElementById("pie-chart"), a );	
+	var pie_chart = new Chart(document.getElementById("pie-chart"), a );
+	pie_chart.destroy();
+	pie_chart = new Chart(document.getElementById("pie-chart"), a );
 
 	
 	var chrome_chart = new Chart(document.getElementById("chrome-chart"), b);
