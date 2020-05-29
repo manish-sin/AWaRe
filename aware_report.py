caac41c4fca87371f250e5fb003586789324e2ec
manish <manish.singh35200@gmail.com>
2020-05-30 02:49:22 +0530

    Complete V1

--- aware_report.py
+++ aware_report.py
@@ -1,5 +1,14 @@
 #take 3 days log,
 #Print
+# Self-Efficacy influences the choices we make,  the efforts we put forth and how we feel.
+# -somewhere on internet
+
+# Keeping this as theme, I and Rahul have wrote a small software Aware. We hoped it would serve as a tool to help us calibrate our self-efficacy. We have been using if for sometime know, and we thought some of you might also find it useful .
+#
+# Hence we have packaged it and made it available as freeware.  This software logs data from your PC and produces a meaningful report. We have made the source code available so that you are sure that your  data is safe.
+#
+# You may know more about Aware and Download it from the below github Link. Do let us know for feedback on github (or here). You may also PM us you mail id, we will inform if there is any update in future
+
 
 # Objective of this Files
 # In file we will write code to generate report form the log of AWaRe The Report will have two charts
@@ -19,21 +28,88 @@ import numpy as np
 from datetime import date
 import time
 import random
+import threading
 
+import win32gui, pandas as pd
 import datetime
+import os
+
 
 import socketio
 from flask import Flask, render_template, jsonify, url_for
 from flask_socketio import SocketIO, send, emit
 from engineio.async_drivers import gevent #do not delet this this is required for packaging
-today = date.today()
 
-date = "2020-05-25"
+today = str(date.today())
+
+date = today
 df_date  = pd.DataFrame()
 
 max_date = "2020-05-25"
 min_date = "2020-05-25"
 #print("hi")
+
+def log_windows():
+    def delete_old_log(df, latest_log):
+        # print(df)
+        index_list = []
+        for i in range(len(df)):
+            check_log = datetime.datetime.strptime(df["Time Stamp"][i], '%Y-%m-%d %H:%M:%S.%f').date()
+            date_diff = (latest_log - check_log).days
+            if (date_diff > 4):
+                index_list.append(i)
+        df.drop(df.index[index_list], axis=0, inplace=True)
+        return df
+    print("hello")
+    aw = win32gui
+    infinity = 0
+    TS = datetime.datetime.now()
+    AWR = "Dummy"
+    i = 0
+    duration = 0
+    AWRF = "abc"
+    if os.path.exists('C:/Aware/awl.csv'):
+        awl = pd.read_csv('C:/Aware/awl.csv', index_col="Unnamed: 0")
+    else:
+        columns = ["Time Stamp", "Window", "Suration"]
+        awl = pd.DataFrame(columns=columns)
+    #awl = awl[awl['Window'] != 'abc']
+    while True:
+        time.sleep(1)
+        AWR = aw.GetWindowText(aw.GetForegroundWindow())
+
+        while AWR != AWRF:
+            # print(i)
+            TS_str = str(TS)
+            to_append = [TS_str, AWRF, i]
+            #print(to_append)
+            TS = datetime.datetime.now()
+            # print(TS_str)
+            i = 0
+            AWRF = AWR
+            awl_length = len(awl)
+            awl.loc[awl_length] = to_append
+            awl.to_csv('C:/Aware/awl.csv')
+
+
+            df = pd.read_csv('C:/Aware/awl.csv', index_col='Unnamed: 0')
+
+            first_log = datetime.datetime.strptime(df["Time Stamp"][0], '%Y-%m-%d %H:%M:%S.%f').date()
+            latest_log = datetime.datetime.strptime(df['Time Stamp'][df.last_valid_index()],
+                                                    '%Y-%m-%d %H:%M:%S.%f').date()
+            date_diff = (latest_log - first_log).days
+            #print(df)
+            if (date_diff > 4):
+                df = delete_old_log(df, latest_log)
+                df= df.reset_index(drop=True)
+                df.to_csv('C:/Aware/awl.csv')
+        i = i + 1
+
+loging = threading.Thread(target=log_windows)
+loging.start()
+
+
+
 
 def generate_colors(app_count):
     colors = []
@@ -45,14 +121,14 @@ def generate_colors(app_count):
 
 def get_date_specific_log(date):
     # Import the csv generated using the aware utility and converting the time string into date time format
-    df = pd.read_csv("C:/Users/manis/Desktop/awl.csv", index_col='Unnamed: 0')  # C:/Users/manis/Desktop/
-    #print(df)
+    df = pd.read_csv('C:/Aware/awl.csv', index_col='Unnamed: 0')  # C:/Users/manis/Desktop/
+    print("date_specific lof", df)
+    #print("THis is Data", date)
     # drop the first row which is used as a marker and to find the time difference between application
-    df = df.drop(0)
-    df = df.reset_index(drop=True)
-    # Fill all the empty cell with Ideal value
-    df.fillna('Ideal', inplace=True)
 
+    # Fill all the empty cell with Ideal value
+
+    #print("THis is Data", date)
     # Step 2: CHART 1
     # Create a datafrme which cab be used to plot chart, in same way way as we do in excel. Alternative suggesion are welcome
     # Convert Datasheet into list
@@ -60,20 +136,42 @@ def get_date_specific_log(date):
     # Filter out the Application name from the window and place them in new window
     # step 1: split the window name to extract application name i.1. the last string after a  "-"
     # Step 2: store the application name in the newly created Column -Application-
+    df['Time Stamp'] = df['Time Stamp'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f'))
+    dp = pd.DataFrame(df['Time Stamp'].diff())
+    dp = dp.drop(0).reset_index(drop=True)
 
+    dp.rename(columns={"Time Stamp": "Duration"}, inplace=True)
+    df = df.join(dp)
+    df.dropna(subset=["Duration"], inplace=True)
+    # df.reset_index(inplace=True)
+    df['Duration'] = df['Duration'].apply(lambda x: int(x.seconds))
+    df.rename(columns={"Duration": "Suration", "Suration": "Dur"}, inplace=True)
+    df['Time Stamp'] = df['Time Stamp'].apply(lambda x: str(x))
+
+
+    df.fillna('Ideal', inplace=True)
+    print(df)
+
+
+
+    print(type(df["Time Stamp"][0]))
     df["Application"] = 0  # new column created
-    # print(df)
+
     index = 0
     for window in df["Window"]:
         app = window.split("-")  # Step 1
         app = app[-1]
         df.loc[index, "Application"] = app  # step2
         index = index + 1
-    # print(df)
-    max_date = df['Time Stamp'][0][:10]
-    min_date = df['Time Stamp'][df.last_valid_index()][:10]
+    print("added application", df)
+    mx_date = datetime.datetime.strptime(df["Time Stamp"][0], '%Y-%m-%d %H:%M:%S.%f').date()
+    max_date = str(mx_date)
+
+    mn_date = datetime.datetime.strptime(df['Time Stamp'][df.last_valid_index()], '%Y-%m-%d %H:%M:%S.%f').date()
+    min_date = str(mn_date)
+
     df = df[df['Time Stamp'].map(lambda x: date in x)]
-    # print(df)
+    print("final",df)
     df_date = df
     # print(df_date)
     return df_date, max_date, min_date
@@ -93,7 +191,7 @@ def chart_of_apps(df):
 
     # Step1: Pivot table
     app_chart1 = pd.pivot_table(df, values="Suration", index=["Application"], aggfunc=np.sum).reset_index()
-    # print("App Function", app_chart1)
+    print("App Function", app_chart1)
     # Threshold duration to conside if an app needs to be put in mislaneous category
     thrashold = 300
     # basic declearation for dataframe manupulation
@@ -101,13 +199,12 @@ def chart_of_apps(df):
     misc = 0
     index = 0
     misc_apps = []  # declearling enpty list to store apps which will be placed in mislaneous category
-    # print(app_chart1)
+    print(app_chart1)
     for item in app_chart1["Suration"]:
         if (item < thrashold):  # Step 2: operation to be done for each row which are leaa than threshold
             if (item > low_threshold):
                 misc = misc + item  # cumulative time of each app
-                misc_app = app_chart1["Application"][
-                    index]  # getting the name of sub-application to store in "misc_apps"
+                misc_app = app_chart1["Application"][index]  # getting the name of sub-application to store in "misc_apps"
                 misc_apps.append(misc_app)  # step4: creating list misc_apps
             app_chart1.drop([index], inplace=True)
         index = index + 1
@@ -255,6 +352,7 @@ def get_chart_data(data):
 
 
 # Define an flask server app
+#
 # print(get_chart_data(3))
 
 app = Flask(__name__)
@@ -269,8 +367,8 @@ def index():
 # set a socket to send message
 @socketio.on('message')
 def message(data):
-    print(f"{data}")
-    print(date)
+    #print(f"{data}")
+    #print(date)
     app_name = "Miscellaneous"  # llaneous"
     chart_list = []
     global df_date
@@ -286,7 +384,7 @@ def message(data):
     chart_list.append(date)
     chart_list.append(app_count)
     data = chart_list
-    print(data)
+    #print(data)
     send(data, broadcast=True)
 
 print("hey")
@@ -305,4 +403,15 @@ def message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug = True, port =5000)
+    socketio.run(app, debug = False, port =5000)
+
+
+
+
+
+
+
+
+
+
+
